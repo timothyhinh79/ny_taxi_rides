8e6c3ad9cbecca7fa0ef769273b4289fa11b6f76
timothyhinh79@gmail.com <timothyhinh79@gmail.com>
2022-07-23 18:39:40 +0000

    Completed DBT Modeling

---
+++ macros/get_payment_type_description.sql
@@ -0,0 +1,16 @@
+{#
+    This macro returns the description of the payment_type
+#}
+
+{% macro get_payment_type_description(payment_type) -%}
+
+    CASE {{payment_type}}
+        WHEN 1 THEN 'Credit card'
+        WHEN 2 THEN 'Cash'
+        WHEN 3 THEN 'No charge'
+        WHEN 4 THEN 'Disupute'
+        WHEN 5 THEN 'Unknown'
+        WHEN 6 THEN 'Voided trip'
+    END 
+
+{%- endmacro %}
---
+++ models/core/dim_zones.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='table') }}
+
+SELECT
+    locationid,
+    borough,
+    zone,
+    replace(service_zone, 'Boro','Green') AS service_zone
+FROM {{ ref('taxi_zone_lookup')}}
